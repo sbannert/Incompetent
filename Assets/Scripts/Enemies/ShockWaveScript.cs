917186dba10132cf72c0fa94e23e9bfcc8d13f35
Scott Bannert <sbannert@gmail.com>
2018-10-14 11:43:40 -0500

    Reset

---
+++ Assets/Scripts/Enemies/ShockWaveScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShockWaveScript : MonoBehaviour {
+
+    [SerializeField]
+    float speed;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        /*if(collision.gameObject.name == "Floor")
+        {
+            Debug.Log("collided with obstacle");
+            Destroy(gameObject);
+        }*/
+    }
+}
---
+++ Assets/Scripts/Enemies/SpikeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikeScript : MonoBehaviour
+{
+    [SerializeField]
+    SpikeTowerScript spikeTow;
+    [SerializeField]
+    float spikeDuration;
+    GameObject mom;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(StartDestroy());
+        spikeTow = FindObjectOfType<SpikeTowerScript>();
+    }
+
+    private IEnumerator StartDestroy()
+    {
+        yield return new WaitForSeconds(spikeDuration);
+        spikeTow = gameObject.GetComponentInParent<SpikeTowerScript>();
+            spikeTow.StartSpike();
+        Destroy(gameObject);
+    }
+}
